76ad9f49be089e6dd4ec0012a28f410e103d75da
filivilla <filiberto.villa2532@my.riohondo.edu>
2024-11-26 09:07:41 -0800

    added comments

--- file.cpp
+++ file.cpp
@@ -7,13 +7,17 @@
 using namespace std;//name change for cin and cout
 
 void file::output(vector<string>& x,vector<int>& bets,ofstream& y)
-{
-    for(size_t i = 0; i < x.size(); ++i)
+{//void function defintion 
+//** vectors passed as string to keep memory short*/
+    for(size_t i = 0; i < x.size(); ++i)//for loops through the number of elements in the vector of names
+    //which is the same size as the bets 
+    //**size_t ensures that it can store the maximum number elemetns for array 
         cout << "Player: " << x[i] << ", Bet: $" << bets[i] << endl;
-}
+        //outputs the elements i for the name and bets vector 
+}//end output
 
 void file::ace(int count[], int &x, int user)
-{
+{//void function definiton 
     if(count[user] + 11 > 21)
         x = 1;
     else
@@ -21,14 +25,15 @@ void file::ace(int count[], int &x, int user)
 }
 
 void file::readfile(ifstream &x, vector<string> &usernames, vector<int> &bets)
-{
-    string first,last;
+{//void function definiton 
+    string first,last;//declared variables 
     int money;
 
     while (x >> first >> last >> money)
-    {
-        string name = first + " " + last;
-        usernames.push_back(name);
-        bets.push_back(money);
-    }
-}
+    {//while continues when file has still information 
+        string name = first + " " + last;//intialized name with first and last
+        usernames.push_back(name);//intserts the new value to the end of the vector
+        bets.push_back(money);//inserts the new value at the end of the vector
+    //**.push_back() inserts a value to the end of the vector */
+    }//end while 
+}//end readfile
--- file.h
+++ file.h
@@ -1,11 +1,15 @@
 //file.h
 
-#include <fstream>
-#include <vector>
-using namespace std;
-class file{
-public:
-    void ace(int count[],int& x,int user);
+#include <fstream>//preprocessing directive for file input and output
+#include <vector>//preproecessing directve for vector usage
+using namespace std;//name change for cin cout
+class file{//class defintion of file
+public://public member functions 
+    void ace(int count[],int& x,int user);//void function prototypes
+        //ace function helps determine what value the ace should be 
     void readfile(ifstream& x, vector<string>& usernames, vector<int>& bets);
+        //readfile function that gets the first name, last name, and the bet from the ifstream
+        //then inputs that info into the vector for the player name and bet
     void output(vector<string>& x,vector<int>& bets,ofstream& y);
+        //ouput function outputs the infromatiinon in the vectors of the players name and bets
 };
